28ce380539bf5dc27b61de75d20716b7fd9f9c1a
Dennis Chan <z5309088@unsw.edu.au>
2023-05-17 13:24:46 +1000

    added navbar to layout, html for about page

---
+++ app/our-mission/page.tsx
@@ -0,0 +1,20 @@
+import './style.css';
+
+export default function ourMission() {
+  return (
+    <body>
+    <div className='section-border'>
+      <div className='content-wrapper'>
+        <h1 id='page-title'>Our Mission</h1>
+      </div>
+    </div>
+    <div className='section-border'>
+      <div className='content-wrapper'>
+        <h2>
+          Our mission is to empower students to take action towards a financially secure future. We hope to help facilitate students with their transition from university to adult life, with specific attention to the area of personal finance as it enables them to better pursue their various goals.
+        </h2>
+      </div>
+    </div>
+  </body>
+  )
+}
